39db0b72f85fcffbf4ccf5e0f2e33ccabdc47929
王家葳 <forme.wjw@aliyun.com>
2019-11-26 15:14:21 +0800

    tools

--- utils.py
+++ utils.py
@@ -3,7 +3,11 @@
 import requests
 import json
 import hashlib
+import base64
+from Crypto.Cipher import PKCS1_v1_5 as Cipher_pkcs2_v1_5
+from Crypto.PublicKey import RSA
 from redis import StrictRedis
+
 
 # 屏蔽warning
 requests.packages.urllib3.disable_warnings()
@@ -17,6 +21,7 @@ def json_loads(js_ctx):
     except:
         print('json加载出错',js_ctx)
     return js_dict
+
 
 # cookie编辑
 def cookie_transform(c_l):
@@ -115,7 +120,7 @@ def hset_name(id_key, field, value):
 
 def hget_name(id_key, name):
     rds = connect_redis()
-    return rds.hget(name=id_key, key=name).decode()
+    return rds.hget(name=id_key, key=name).decode('utf-8')
 
 
 def get_current_cookie(id_key):
@@ -140,6 +145,21 @@ def calculate_md5(ctx):
 # cookie_deal
 def cookie_dealer(id_key):
     cookies_list = get_current_cookie(id_key)
+    cookies_list = sorted(cookies_list)
     cookies = cookie_transform(cookies_list)
     return cookies_list, cookies
 
+
+# cookie maker
+def cookie_maker(c_l, n_c):
+    return cookie_transform(session_cookie_update(c_l, n_c))
+
+
+# RSA
+def encrpt_pwd(pwd):
+    key = """-----BEGIN PUBLIC KEY-----
+    MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsgDq4OqxuEisnk2F0EJFmw4xKa5IrcqEYHvqxPs2CHEg2kolhfWA2SjNuGAHxyDDE5MLtOvzuXjBx/5YJtc9zj2xR/0moesS+Vi/xtG1tkVaTCba+TV+Y5C61iyr3FGqr+KOD4/XECu0Xky1W9ZmmaFADmZi7+6gO9wjgVpU9aLcBcw/loHOeJrCqjp7pA98hRJRY+MML8MK15mnC4ebooOva+mJlstW6t/1lghR8WNV8cocxgcHHuXBxgns2MlACQbSdJ8c6Z3RQeRZBzyjfey6JCCfbEKouVrWIUuPphBL3OANfgp0B+QG31bapvePTfXU48TYK0M5kE+8LgbbWQIDAQAB
+    -----END PUBLIC KEY-----"""
+    rsakey = RSA.importKey(key)
+    cipher = Cipher_pkcs2_v1_5.new(rsakey)
+    return base64.b64encode(cipher.encrypt(pwd.encode())).decode('utf-8')
