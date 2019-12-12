c1094275d39dc0196273307fcd8e9753264e31c5
王家葳 <forme.wjw@aliyun.com>
2019-12-12 15:58:24 +0800

    配置文件

--- loginModel/__init__.py
+++ loginModel/__init__.py
@@ -1,34 +1,60 @@
 # coding=utf-8
 
+"""该模块承担的是
+移动，电信，联通
+的登录逻辑模块
 
-"""
-    针对三大运营商的通话详情数据获取模块
-    这里仅仅是一个demo
-    感兴趣的，获取别的数据都行
-    哪怕通过爬虫修改自己的套餐都行
-    会出现没有数据的情况，那是服务器自己抽风
+**目前仅仅支持四川**
+# ###########################
+# ###### cmcc代表 中国移动 ##
+# ###### cu代表 中国联通 ##
+# ###### ct代表 中国电信 ####
+# ##########################
+
+每一次请求进来，颁发一个身份证，cookie信息都存储在redis对应的身份证里
+这样做的目的在于，对于放到线上接口且高并发的情况下不容易错
+身份证为 手机号+时间戳 进行md5转换
 """
 
-from .config import DETAIL_URL
-from .config import DETAIL_HEADERS
-from .config import DETAIL_PAYLOADS
-from .config import DETAIL_PARAMS
-from utils import hget_name
-from utils import hset_name
-from utils import cookie_dealer
+
+import requests
 from utils import cookie_transform
+from utils import connect_redis
+from utils import hset_name
 from utils import get_request
 from utils import post_request
-from utils import session_cookie_update
-from utils import save_new_cookie
 from utils import get_request_cu
 from utils import post_request_cu
+from utils import save_new_cookie
+from utils import cookie_dealer
+from utils import hget_name
+from utils import session_cookie_update
 from utils import json_loads
-from .js_hacker import COLLECT_ID
-from .js_hacker import WTFPC
-from .ct_detail_handler import ct_detail_run
-from .cmcc_detail_handler import cmcc_detail_run
-from .cu_detail_handler import cu_detail_run
+from utils import encrpt_pwd
+from .config import PROV_LIST
+from .config import LOGIN_URL
+from .config import LOGIN_HEADERS
+from .config import LOGIN_PARAMS
+from .config import LOGIN_PAYLOADS
+from .js_hacker import TRKID_JS
+from .js_hacker import CODE_V
+from .js_hacker import LVID_JS
+from .js_hacker import S_FID_JS
+from .js_hacker import NVID
+from .js_hacker import TRKHMCLICKCOORDS
+from .js_hacker import SVID_URL
+from .js_hacker import SVID_HEADERS
+from .js_hacker import SVID_PARAMS
+from .js_hacker import ECS_REQINFO_LOGIN1
+from .js_hacker import ASE
+from .js_hacker import DCSFPC
+from .js_hacker import N3FA_CID
+from .js_hacker import N3FA_EXT
+from .js_hacker import N3FA_LVT
+from .js_hacker import N3FA_LPVT
+from .ct_login_handler import ct_run
+from .cmcc_login_handler import cmcc_run
+from .cu_login_handler import cu_run
 
 
 def receive_key_do_switcher(id_key):
@@ -44,8 +70,8 @@ def receive_key_do_switcher(id_key):
 
 def sp_switcher(sp):
     sp_dict = {
-        'cmcc': cmcc_detail_run,
-        'ct': ct_detail_run,
-        'cu': cu_detail_run,
+        'cmcc': cmcc_run,
+        'ct': ct_run,
+        'cu': cu_run,
     }
     return sp_dict.get(sp)
